037a18ee3249a9f56480ef96f3eb1c211a51862e
MarcoSac1 <marcosaccone8@gmail.com>
2024-11-29 15:54:53 +0100

    aggiunta consegna

--- AStrisce.java
+++ AStrisce.java
@@ -1,3 +1,4 @@
+// Si scriva un metodo “aStrisce” che riceve in input una matrice di interi e restituisce true se e solo se la matrice è a strisce orizzontali, cioè se le righe di indice dispari contengono solo zeri, mentre le righe pari non contengono zeri.
 public class AStrisce {
     public static boolean[] aStrisce(int M[][]) {
         // Array per memorizzare il risultato per ogni riga
@@ -25,8 +26,8 @@ public class AStrisce {
     public static void main(String[] args) {
         // Esempio di matrice
         int[][] matrice = {
-            {0, 0, 0},       // Riga 1: valida
             {1, 3, 5},       // Riga 0: valida
+            {0, 0, 0},       // Riga 1: valida
             {0, 0, 0},       // Riga 3: valida
             {0, 0, 0},     // Riga 4: valida
             {7, 9, 11}      // Riga 2: valida
