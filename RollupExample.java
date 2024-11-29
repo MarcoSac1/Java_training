2f0938f22b51c56bc5f3564af661b9be08d8a438
MarcoSac1 <marcosaccone8@gmail.com>
2024-11-29 11:13:51 +0100

    uncomented code

--- RollupExample.java
+++ RollupExample.java
@@ -1,29 +1,29 @@
-// public class RollupExample {
-//     // Metodo rollup
-//     public static int[] rollup(int[] l) {
-//         // Creazione di un array con la metà della lunghezza di l
-//         int[] ris = new int[l.length / 2];
+public class RollupExample {
+    // Metodo rollup
+    public static int[] rollup(int[] l) {
+        // Creazione di un array con la metà della lunghezza di l
+        int[] ris = new int[l.length / 2];
         
-//         // Ciclo per calcolare la somma delle coppie
-//         for (int i = 0; i < l.length / 2; i++) {
-//             ris[i] = l[2 * i] + l[2 * i + 1];
-//         }
+        // Ciclo per calcolare la somma delle coppie
+        for (int i = 0; i < l.length / 2; i++) {
+            ris[i] = l[2 * i] + l[2 * i + 1];
+        }
         
-//         return ris; // Restituisce l'array risultato
-//     }
+        return ris; // Restituisce l'array risultato
+    }
 
-//     // Metodo main per far partire il programma
-//     public static void main(String[] args) {
-//         // Array di esempio
-//         int[] input = {1, 2, 3, 4, 5, 6, 7, 8};
+    // Metodo main per far partire il programma
+    public static void main(String[] args) {
+        // Array di esempio
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8};
         
-//         // Chiamata al metodo rollup
-//         int[] result = rollup(input);
+        // Chiamata al metodo rollup
+        int[] result = rollup(input);
 
-//         // Stampa del risultato
-//         System.out.print("Risultato: ");
-//         for (int value : result) {
-//             System.out.print(value + " ");
-//         }
-//     }
-// }
+        // Stampa del risultato
+        System.out.print("Risultato: ");
+        for (int value : result) {
+            System.out.print(value + " ");
+        }
+    }
+}
